62cfb689fc631d2c592872257731ba6f922aa0bd
tumble <tumblegamer@gmail.com>
2023-07-05 17:33:33 +0100

    epg buttons

---
+++ assets/js/epg.js
@@ -0,0 +1,15 @@
+function pressTvGuide() {
+	window.location.href = "https://stb-gaming.github.io/sky-guide/tv-guide/";
+}
+
+function pressBoxOffice() {
+	window.location.href = "https://stb-gaming.github.io/sky-guide/box-office/";
+}
+
+function pressServices() {
+	window.location.href = "https://stb-gaming.github.io/sky-guide/services/";
+}
+
+function pressInteractive() {
+	window.location.href = "https://stb-gaming.github.io/sky-guide/intractive/";
+}
