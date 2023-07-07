3d3cf013e59084c26b6b269280d01b525714a7fa
tumble <tumblegamer@gmail.com>
2023-07-07 14:21:34 +0100

    youtube test

--- assets/js/sky-tv.js
+++ assets/js/sky-tv.js
@@ -1,7 +1,27 @@
 let videos = Array.from(document.getElementsByClassName("video-container"));
 console.log(`Found ${videos.length} videos`);
+
+let ytSetup = false;
+function setupYT() {
+	if (ytSetup) return;
+
+	window.onYouTubeIframeAPIReady = function () {
+		console.log("YouTube Ready");
+		document.dispatchEvent(new Event("youtubeready"));
+	};
+
+	var tag = document.createElement('script');
+
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	ytSetup = true;
+	console.log("YouTube Player API Setup");
+}
+
 videos.forEach(container => {
 	let video = container.querySelector("video"),
+		youtube = container.querySelector("iframe"),
 		playPause = container.querySelector(".play-pause"),
 		mute = container.querySelector(".mute"),
 		fullscreen = container.querySelector(".fullscreen"),
@@ -9,12 +29,14 @@ videos.forEach(container => {
 		visibilityTimeout;
 	if (playPause) {
 		playPause.addEventListener("click", () => {
+			if (!video) return;
 			video.paused ? video.play() : video.pause();
 		});
 		console.log("added play button functionality");
 	}
 	if (mute) {
 		mute.addEventListener("click", () => {
+			if (!video) return;
 			video.muted = !video.muted;
 		});
 		console.log("added mute button functionality");
@@ -35,10 +57,10 @@ videos.forEach(container => {
 		console.log("added fullscreen button functionality");
 	}
 
-
-	video.addEventListener("canplay", () => {
-		console.log("Video Ready");
-	});
+	if (video)
+		video.addEventListener("canplay", () => {
+			console.log("Video Ready");
+		});
 
 	function makeControlsVisiable() {
 		if (visibilityTimeout) clearTimeout(visibilityTimeout);
@@ -54,5 +76,52 @@ videos.forEach(container => {
 	});
 	makeControlsVisiable();
 
-	window.TEST_VIDEO = video;
+	if (!video && !!youtube) {
+		controls.style.display = "none";
+		document.addEventListener("youtubeready", () => {
+
+			let player = new YT.Player(youtube.id, {
+
+				playerVars: {
+					'playsinline': 1
+				},
+				events: {
+					'onStateChange': (event) => {
+						if (event.data != -1) {
+							controls.style.display = null;
+
+						}
+
+					}
+				}
+			});
+			console.log("Connected player to youtube player api");
+			window.YOUTUBE_TEST = player;
+
+			window.CONTROLS = controls;
+
+
+			video = {
+				play: () => {
+					player.playVideo();
+				},
+				pause: () => {
+					player.pauseVideo();
+				},
+				get paused() {
+					return [-1, 2, 5, 0].includes(player.getPlayerState());
+				},
+				get muted() {
+					return player.isMuted();
+				},
+				set muted(value) {
+					if (value)
+						player.mute();
+					else
+						player.unMute();
+				}
+			};
+		});
+		setupYT();
+	}
 });
